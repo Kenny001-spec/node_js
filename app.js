3c93522af3b1c236096c9ff075e15e54bbbca320
kehinde01 <abubakarkehinde88@gmail.com>
2022-08-05 18:25:12 +0100

    initial commit

--- app.js
+++ app.js
@@ -1,30 +1,60 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
+const { query } = require('express');
 
 
 const app = express();
+app.set('view engine', 'hbs')
 
 
 const publicDirectory = path.join(__dirname, './public');
 app.use(express.static(publicDirectory));
 
+const viewsPath = path.join(__dirname, './templates/views');
+const partialsPath = path.join(__dirname, './templates/partials');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+// console.log(viewsPath);
 
 
-const port = 2010;
 
+const port = 2000;
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>This is the Homepage</h1>');
+// });
 
 app.get('/', (req, res) => {
-    res.send('<h1>This is the Homepage</h1>');
-});
-
-
+    res.render('index', {
+        app: 'MISSION & VISSION STATEMENT 🌎✌',
+        page: 'Im a web Developer ✍'
+    });
+})
 app.get('/about', (req, res) => {
-    res.send('<h1>This is the About page</h1>');
-});
+    res.render('about', {
+        app: 'Web Development Project Ideas 🌎💪',
+        page: 'About page'
+    });
+})
+
+
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>This is the About page</h1>');
+// });
 
 
 app.get('/api', (req, res) => {
-    res.send({ name: 'Olamide', location: 'canada' });
+    if (!req.query.search) {
+        res.send({
+            err: 'You have to put a search term'
+        })
+    } else {
+        // console.log(req.query.search);
+        res.send({ name: 'Olamide', location: 'canada' });
+    }
+
 });
 
 
